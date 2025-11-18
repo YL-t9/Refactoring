6d6883668c8ee064c16cdfb995ae6365021d1ee5
Yuan <yuanlu.lu@mail.utoronto.ca>
2025-11-17 19:05:47 -0500

    finish Task 1.1 and pass the test

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,35 @@
 package theater;
 
+/**
+ * Represents a theatrical play with a name and type.
+ * Used to store basic metadata about a performance.
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+    /**
+     * Returns the name of this play.
+     *
+     * @return the play name (may be null)
+     */
+
+    public String getName() {
+        return name;
+    }
+    /**
+     * Returns the type of this play.
+     *
+     * @return the type of the play (maybe null)
+     */
+
+    public String getType() {
+        return type;
     }
 }
